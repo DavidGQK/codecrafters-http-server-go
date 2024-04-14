a8a722cb1a05f016f9064ad206c0783d87bfdb24
David <davka8989@gmail.com>
2024-04-14 18:07:12 +0300

    pass stage

--- app/utils.go
+++ app/utils.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 )
 
 func findFile(dir, filename string) (string, bool) {
-	currPath, _ := os.Getwd()
-	fmt.Println("curr_path:", currPath)
-
 	fPath := filepath.Join(dir, filename)
-	fmt.Println("filepath:", fPath)
 	content, err := os.ReadFile(fPath)
 	if err != nil {
 		return "", false
